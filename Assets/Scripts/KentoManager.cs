9dc1dd88acf617bef4edefb2a4dfde3217645e64
fififuchun <windkindstar20030205@gmail.com>
2023-09-25 14:35:30 +0900

    4つのフォントを実装

---
+++ Assets/Scripts/KentoManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KentoManager : MonoBehaviour
+{
+    public static KentoManager instance;
+    void Awake() { instance = this; }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void PushRotateButton(){
+        // KentoManager.instance.gameObject.transform.rotation= new Vector3(0,0,45);
+    }
+}
