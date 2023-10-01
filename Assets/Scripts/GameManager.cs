2026f7361dc0f2b6190f037634d481f425c05868
fififuchun <windkindstar20030205@gmail.com>
2023-10-01 21:58:37 +0900

    buttonを使わずにドラッグによってkentoを生成

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,43 +5,96 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    //canvas
-    public GameObject canvas;
-
     //すべてのフォントをここに入れる
-    public List<GameObject> allFontGameObjects = new List<GameObject>();
+    [SerializeField] private List<GameObject> allFontGameObjects = new List<GameObject>();
 
     //1ゲーム限りのallFontGameObjectsから取り出したランダムな持ちフォント
-    public GameObject[] myGameObjects = new GameObject[6];
+    [SerializeField] private GameObject[] myGameObjects = new GameObject[6];
 
-    //置かれたゲームオブジェクトを追加
-    public List<GameObject> placedGameObjects = new List<GameObject>();
+    //置かれたゲームオブジェクトのKentoManagerインスタンスを格納
+    [SerializeField] private List<KentoManager> placedGameObjects = new List<KentoManager>();
 
-    public bool canInstantiate;
+    //phase管理
+    Phase phase;
+    enum Phase
+    {
+        //一応、一度きり
+        AwakePhase,
+        StartPhase,
+
+        //次の検討の出現、一度きり
+        AppearPhase,
+
+        //落ちてる状態、いらない？
+        PutPhase,
+
+        //スピードの判定、スピードが落ち着くまで
+        // JudgePhase,
+    }
+
+    //canvas
+    public GameObject canvas;
 
     void Start()
     {
         canvas = GameObject.Find("Canvas");
-        PutKento();
+        StartCoroutine(Loop());
+    }
+
+    IEnumerator Loop()
+    {
+        while (true)
+        {
+            Debug.Log(phase);
+            switch (phase)
+            {
+                case Phase.AwakePhase:
+                    PutKento();
+                    phase = Phase.AppearPhase;
+                    break;
+                case Phase.StartPhase:
+                    yield return new WaitForSeconds(1f);
+                    phase = Phase.AppearPhase;
+                    break;
+                case Phase.AppearPhase:
+                    yield return new WaitUntil(() => !placedGameObjects.Last().GetComponent<KentoManager>().enabled);
+                    PutKento();
+                    phase = Phase.PutPhase;
+                    break;
+                case Phase.PutPhase:
+                    yield return new WaitForSeconds(1f);
+                    phase = Phase.StartPhase;
+                    break;
+                // case Phase.JudgePhase:
+                //     // yield return new WaitForSeconds(1f);
+                //     phase = Phase.StartPhase;
+                //     break;
+            }
+        }
     }
 
     void Update()
     {
-        for (int i = 0; i < placedGameObjects.Count; i++) if (placedGameObjects[i].transform.position.y < -1000)
-            {
-                // placedGameObjects.
-        }
+        for (int i = 0; i < placedGameObjects.Count; i++) if (placedGameObjects[i] == null) placedGameObjects.RemoveAll(item => item == null);
     }
 
     public void PutKento()
     {
         int randomNum = Random.Range(0, 6);
-        GameObject kentoPrefab = Instantiate(myGameObjects[randomNum], new Vector3(0, 300, 0) + canvas.transform.position, Quaternion.identity, canvas.transform);
-        placedGameObjects.Add(kentoPrefab);
+        GameObject kentoPrefab = Instantiate(myGameObjects[randomNum], new Vector3(0, 600, 0) + canvas.transform.position, Quaternion.identity, canvas.transform);
+        kentoPrefab.GetComponent<RectTransform>().sizeDelta *= Mathf.Pow(2, Random.Range(-1, 2));
+        placedGameObjects.Add(kentoPrefab.GetComponent<KentoManager>());
     }
+
+    // public bool CanInstantiate(KentoManager kento)
+    // {
+    //     if (kento.KentoSpeed > 1) return false;
+    //     return true;
+    // }
 
     public void PushRotateButton()
     {
+
         placedGameObjects.Last().transform.Rotate(new Vector3(0, 0, 45));
     }
 }
--- Assets/Scripts/KentoManager.cs
+++ Assets/Scripts/KentoManager.cs
@@ -1,43 +1,80 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class KentoManager : MonoBehaviour
+public class KentoManager : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
-    public static KentoManager instance;
-    void Awake() { instance = this; }
+    //親スクリプトの取得
+    private GameManager gameManager;
 
     // 1フレーム前の位置
-    private Vector3 _prevPosition;
+    // private Vector3 _prevPosition;
+
     // 現在のスピード
-    public float kentoSpeed;
+    // [SerializeField] private float kentoSpeed;
+    //kentoSpeedのプロパティ
+    // public float KentoSpeed { get => kentoSpeed; }
+
+    //保存しておく初期position
+    private Vector2 prevPos;
+
+    // 移動したいオブジェクトのRectTransform
+    private RectTransform rectTransform;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
 
     private void Start()
     {
         // 初期位置を保持
-        _prevPosition = transform.position;
+        // _prevPosition = transform.position;
     }
 
     private void Update()
     {
-        // if (gameObject.transform.position.y < -1500) Destroy(gameObject);
+        //kentoの管理はこっち
+        if (transform.position.y < -1000) Destroy(gameObject);
 
         // deltaTimeが0の場合は何もしない
-        if (Mathf.Approximately(Time.deltaTime, 0)) return;
+        // if (Mathf.Approximately(Time.deltaTime, 0)) return;
 
         // 現在位置取得
-        var position = transform.position;
+        // var position = transform.position;
 
         // 現在速度計算
-        var velocity = (position - _prevPosition) / Time.deltaTime;
-        kentoSpeed = velocity.magnitude;
-        Debug.Log(kentoSpeed);
-
-        // 現在速度をログ出力
-        // Debug.Log($"velocity = {velocity}");
+        // var velocity = (position - _prevPosition) / Time.deltaTime;
+        // kentoSpeed = velocity.magnitude;
+        // Debug.Log(kentoSpeed);
 
         // 前フレーム位置を更新
-        _prevPosition = position;
+        // _prevPosition = position;
+    }
+
+    // ドラッグ開始時の処理
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        // ドラッグ前の位置を記憶しておく、RectTransformの場合はpositionではなくanchoredPositionを使う
+        prevPos = rectTransform.anchoredPosition;
+        Debug.Log("drag開始" + prevPos);
+    }
+
+    // ドラッグ中の処理
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = new Vector3(eventData.position.x, prevPos.y + gameManager.canvas.transform.position.y);
+    }
+
+    // ドラッグ終了時の処理
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        gameObject.GetComponent<Rigidbody2D>().gravityScale = 20;
+        gameObject.GetComponent<KentoManager>().enabled = false;
+        Debug.Log("drag終了");
     }
 }
