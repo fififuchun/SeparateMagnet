67e1cf97cb331f4e9c138757c39056b7a19effcf
catsoba <yuuking.ogu.2004@gmail.com>
2023-10-04 18:59:39 +0900

    少し整えた

--- Assets/ExpBer.cs
+++ Assets/ExpBer.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 using UnityEngine.UI;
 
-public class exp_ber : MonoBehaviour
+public class ExpBer : MonoBehaviour
 {
     int maxExp = 100;//最大の経験値量（暫定レベルアップ時リセット）
     int currentExp = 50;//今の経験値量
