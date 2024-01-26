1d91d27b8173b2e6bbc4fdc6ecd1942327ba1b8c
fififuchun <windkindstar20030205@gmail.com>
2024-01-26 21:51:10 +0900

    Buttonの改修

---
+++ Assets/Scripts/General/CustomButton.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+
+public class CustomButton : MonoBehaviour,
+    IPointerClickHandler,
+    IPointerDownHandler,
+    IPointerUpHandler
+{
+    public System.Action onClickCallback;
+
+    [SerializeField] private CanvasGroup _canvasGroup;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        onClickCallback?.Invoke();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        transform.DOScale(0.95f, 0.24f).SetEase(Ease.OutCubic);
+        _canvasGroup.DOFade(0.8f, 0.24f).SetEase(Ease.OutCubic);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        transform.DOScale(1f, 0.24f).SetEase(Ease.OutCubic);
+        _canvasGroup.DOFade(1f, 0.24f).SetEase(Ease.OutCubic);
+    }
+}
