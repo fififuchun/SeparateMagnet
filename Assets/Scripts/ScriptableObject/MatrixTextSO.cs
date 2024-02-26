b7fb016487c1e2c845db9b179cd60533833c45f7
fififuchun <windkindstar20030205@gmail.com>
2024-02-26 20:18:20 +0900

    MatrixTextが完成したので、初回チュートリアルを作成しました

--- Assets/Scripts/ScriptableObject/MatrixTextSO.cs
+++ Assets/Scripts/ScriptableObject/MatrixTextSO.cs
@@ -2,15 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 [CreateAssetMenu(fileName = "MatrixText", menuName = "Create MatrixTextSO"), System.Serializable]
 public class MatrixTextSO : ScriptableObject
 {
-    public MatrixTextData[] stringGroups; //= new List<MatrixTextData>();
+    public MatrixTextData[] stringGroups;
 }
 
 //表示用のクラス
 [System.Serializable]
 public class MatrixTextData
 {
-    [TextArea] public string[] strings;
+    [TextArea(3, 10)] public string[] strings;
 }
+
+#if UNITY_EDITOR
+[CanEditMultipleObjects]
+[CustomEditor(typeof(MatrixTextSO), true)]
+public class MatrixTextSOEditor : Editor
+{
+    public MatrixTextSO matrixTextSO;
+
+    private void OnEnable()
+    {
+        matrixTextSO = target as MatrixTextSO;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+    }
+}
+#endif
